04c48a72cdffa49ce30691b115b8284eb4f32a30
Kanishka Rani <kanishka22043@gmail.com>
2025-06-12 18:48:05 +0530

    add extra required files

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+# // creating funciton so taht setup will take libraries
+# from requirements .txt and install them 
+def get_requirements(file_path:str)->List[str]:
+    """Returns a list of requirements from a requirements.txt file."""
+    requirements=[]
+    with open (file_path) as f:
+        # \n will also get read by this function
+        requirements=f.readlines()
+        # removing the \n from the end of each line
+        requirements=[r.replace("\n","") for r in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+        
+    return requirements   
+
+
+setup(
+name='Mlproject',
+version='0.0.1',
+author='Kanishka',
+author_email='kanishka22043@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
+
